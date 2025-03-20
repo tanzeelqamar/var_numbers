aa265ec6e311339c6ca8fa88285ce80639837496
Tanzeel Qamar <165699086+tanzeelqamar@users.noreply.github.com>
2025-03-20 14:35:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//age declaration
+var age = 23;
+alert(`I am ${age} years old`)
+
+//visit count
+var visitCount =1;
+alert(`you have visited this site ${visitCount} times`)
+var visitCount1 =2;
+alert(`you have visited this site ${visitCount1} times`)
+
+//birth year
+var birthYear = 2002
+document.write(`My birth year is ${birthYear}\n\n`) 
+
+//clothing store
+var name="John"
+var order=5
+var storeName="'xyz clothing store'"
+document.write(`${name} ordered ${order} T-shirts from ${storeName}`) 
+
+
